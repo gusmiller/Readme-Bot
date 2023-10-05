2d64c10ead629cd3032cfb1e48ddb7dde675f970
Gus Miller <gustavo.miller@miller-hs.com>
2023-10-05 14:14:20 -0400

    Prepare json file and remove un-needed files

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Carleton Bootcamp - 2023
+ * Copyright 2023 Gustavo Miller
+ * Licensed under MIT
+ * Assignment - 09 Readme Bot
+ * Date : 10/5/2023 2:06:08 PM
+ */
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+/**
+ * Initialize arrays and other global requirements to be executed on load.
+ */
+function init() {
+      
+}
+
+/**
+ * Entry point for the application. File will trigger this when it finishes
+ * loading
+ */
+init();
